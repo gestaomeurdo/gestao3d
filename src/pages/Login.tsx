8643560366ce82b84996a50dd1ed52d18d5c78ab
[dyad] <gestao@meurdo.com.br>
2026-05-05 23:18:54 -0300

    [dyad] Orientação sobre ativação do login Google no Supabase - wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -37,6 +37,7 @@ export default function Login() {
           <Auth
             supabaseClient={supabase}
             providers={['google']}
+            redirectTo={window.location.origin}
             appearance={{
               theme: ThemeSupa,
               variables: {
